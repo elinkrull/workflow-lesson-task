e2fa6beb02035d830413e5bf689d86a84e6cf710
elinjakobsen <126721502+elinkrull@users.noreply.github.com>
2025-09-10 18:09:50 +0200

    Fetch API and display five first posts

--- workflow-lesson-task/src/main.js
+++ workflow-lesson-task/src/main.js
@@ -1,24 +1,31 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const app = document.querySelector("#app");
 
-setupCounter(document.querySelector('#counter'))
+app.innerHTML = `
+  <h1>Hello Vite!</h1>
+  <p>Fetching posts...</p>
+`;
+
+// Get API
+const apiUrl = import.meta.env.VITE_API_URL;
+console.log("API URL:", apiUrl);
+
+fetch(apiUrl)
+  .then((res) => res.json())
+  .then((data) => {
+    // Display first five posts
+    app.innerHTML = `
+      <h1>Posts from API</h1>
+      <ul>
+        ${data
+          .slice(0, 5)
+          .map((post) => `<li>${post.title}</li>`)
+          .join("")}
+      </ul>
+    `;
+  })
+  .catch((err) => {
+    console.error("Error fetching:", err);
+    app.innerHTML = `<p style="color:red;">Could not fetch posts</p>`;
+  });
