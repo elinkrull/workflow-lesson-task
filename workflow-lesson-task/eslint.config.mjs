a71deee721e4ae34fde8abd5414a855dd2704024
elinjakobsen <126721502+elinkrull@users.noreply.github.com>
2025-09-10 18:27:11 +0200

    Update config

--- workflow-lesson-task/eslint.config.mjs
+++ workflow-lesson-task/eslint.config.mjs
@@ -1,8 +1,15 @@
 import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
 
 export default [
   js.configs.recommended,
+  prettier,
   {
+    languageOptions: {
+      globals: {
+        ...js.environments.browser.globals,
+      },
+    },
     rules: {
       "no-unused-vars": "warn",
       "no-console": "off",
